bd6c5ca56c9d5184c7c26f59ddd24c0138886581
garveymt <garvey.morgant@gmail.com>
2021-01-23 16:02:30 -0700

    added foodTruckRating and averageRating. Put parts into main as well.

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,26 +1,35 @@
 package com.skilldistillery.foodtruck;
 
-import java.util.Scanner;
-import java.util.UUID;
+//import java.util.Scanner;
+//import java.util.UUID;
 
 public class FoodTruck {
-	int i = 0;
+//	private static final String foodTruckRating = null;
 	String [] foodTruck = {"Fast and the Hungriest", "6 Wheelier Sandwich", "Taco Truck", "Fish On The Go", "One Stop Cookie Shop"};
 	
 public boolean foodTruckList(String whatWasInput, String[] foodTruck) {
+	int i = 0;
 	boolean foodTruckList = true;
 	for(i = 0; i < 5; i++) {
 		if(whatWasInput.equals("Next") || whatWasInput.equals("next")) {
+			System.out.println(foodTruck[0]);
+			System.out.println("To see more options enter \"Next\" or enter \"Quit\" to see ratings and to select a truck for food.");
+		}
+		if(whatWasInput.equals("Next") || whatWasInput.equals("next")) {
 			System.out.println(foodTruck[0] + foodTruck[1]);
+			System.out.println("To see more options enter \"Next\" or enter \"Quit\" to see ratings and to select a truck for food.");
 		}
 		if(whatWasInput.equals("Next") || whatWasInput.equals("next")) {
 			System.out.println(foodTruck[0] + foodTruck[1] + foodTruck[2]);
+			System.out.println("To see more options enter \"Next\" or enter \"Quit\" to see ratings and to select a truck for food.");
 		}
 		if(whatWasInput.equals("Next") || whatWasInput.equals("next")) {
 			System.out.println(foodTruck[0] + foodTruck[1] + foodTruck[2] + foodTruck[3]);
+			System.out.println("To see more options enter \"Next\" or enter \"Quit\" to see ratings and to select a truck for food.");
 		}
 		if(whatWasInput.equals("Next") || whatWasInput.equals("next")) {
 			System.out.println(foodTruck[0] + foodTruck[1] + foodTruck[2] + foodTruck[3] + foodTruck[4]);
+			System.out.println("To see more options enter \"Next\" or enter \"Quit\" to see ratings and to select a truck for food.");
 		}
 		else if (whatWasInput.equals("Quit") || whatWasInput.equals("quit")) {
 			
@@ -45,7 +54,9 @@ public void foodTruckRating(String whatWasInput, char[] foodTruck) {
 	
 }
 public void averageRating() {
-	
+	double average = 0;
+	average = (3 + 4 + 2 + 1 + 5) / 5;
+	System.out.println("The average rating of the food trucks is" + average);
 }
 
 }
